977d616fe072c64f4baf61263215dee47ed898fb
akash084 <akashbaniya084@gmail.com>
2025-08-03 22:01:19 +1000

    Added Navbar

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,34 @@
+import { Icon } from "@iconify/react";
+import logo from "../../assets/Logo.svg"; // ✅ Correct default import for image
+
+const Navbar = () => {
+	return (
+		<>
+			<div className="navBar flex flex-row items-center justify-between mx-[120px] my-2">
+				<div className="logo">
+					<img src={logo} alt="Logo" /> {/* ✅ Don't use quotes around logo */}
+				</div>
+				<div className="navLinks flex flex-row gap-[16px]">
+					<a href="#" className="font-bold text-[#636AE8]">
+						Home
+					</a>
+					<a href="#">Products</a>
+					<a href="#">About</a>
+				</div>
+				<div className="navIcons flex flex-row gap-[16px]">
+					<Icon className="icon" icon="tabler:search" />
+					<Icon
+						// className="opacity-80"
+						icon="solar:user-bold"
+						width="18"
+						height="18"
+					/>
+					<Icon icon="ion:cart" width="18" height="18" />
+					<Icon icon="tabler:moon-filled" width="18" height="18" />
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default Navbar;
